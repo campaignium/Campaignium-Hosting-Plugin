237e87448fd9fec126ed85d2822157122600e565
Keatyn Garton <keatyngarton@Keatyns-MBP.localdomain>
2025-05-02 16:43:37 -0500

    Added version

--- campaignium-hosting.php
+++ campaignium-hosting.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Campaignium Hosting
 Description: Hides the WP Engine dashboard menu for all users except those with an email ending in @campaignium.com.
-Version: 1.0.4
+Version: 1.0.5
 Author: Campaignium
 */
 
